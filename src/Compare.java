f7a707cfc0e1e8180eed433401ed7234d1cafec1
JohnalKhow <johnal_khow@dlsu.edu.ph>
2020-01-30 15:34:34 +0800

    Added printing of correlation matrix

--- src/Compare.java
+++ src/Compare.java
@@ -1,45 +1,21 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.text.DecimalFormat;
-import java.util.Scanner;
 
 public class Compare {
-    private static double similar;
-    private static double total;
-    private static DecimalFormat two_point = new DecimalFormat("#.##");
+    private static DecimalFormat two_point = new DecimalFormat("#,##0.00");
 
-    private static void read_files(String file_ext) {
-        String data1, data2;
-        try {
-            File first_program = new File("Directory/test_program1" + file_ext);
-            File second_program = new File("Directory/test_program2" + file_ext);
-            Scanner first_cmp = new Scanner(first_program);
-            Scanner second_cmp = new Scanner(second_program);
-            while (first_cmp.hasNextLine() || second_cmp.hasNextLine()) {
-                data1 = first_cmp.nextLine();
-                data2 = second_cmp.nextLine();
-                total++;
-                if (data1.equals(data2)) {
-                    similar++;
-                }
-            }
-            first_cmp.close();
-            second_cmp.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("FILE I/O EXCEPTION");
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) {
-        double percentage;
-        read_files(".cpp");
-        System.out.print("Hello");
-        percentage = (similar / total) * 100;
-        System.out.print("\nCPP Similarity: " + two_point.format(percentage) + "%");
-        read_files(".java");
-        percentage = (similar / total) * 100;
-        System.out.print("\nJAVA Similarity: " + two_point.format(percentage) + "%");
+        double [][]correlation_matrix;
+        int count=0;
+        Read file = new Read();
+        Directory_checker dir= new Directory_checker();
+        count = dir.count_files();
+        correlation_matrix=file.read_files(".java");
+        for(int i=0; i<count; i++){
+            for(int j=0; j<count; j++){
+                    System.out.print(two_point.format(correlation_matrix[i][j])+"\t");
+                }
+            System.out.println();
+        }
     }
 }
-
