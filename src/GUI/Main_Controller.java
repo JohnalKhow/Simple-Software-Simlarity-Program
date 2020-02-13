74c849dae1f7c0cd65c74aea11652325caa3c319
JohnalKhow <johnal_khow@dlsu.edu.ph>
2020-02-13 09:38:26 +0800

    Fixed GUI bugs-

--- src/GUI/Main_Controller.java
+++ src/GUI/Main_Controller.java
@@ -7,11 +7,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 import java.io.File;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main_Controller {
     @FXML private GridPane Correlation_matrix;
@@ -19,15 +19,14 @@ public class Main_Controller {
     @FXML
     private void handle_btnCHECK(ActionEvent event){
         Read read= new Read();
-
         Directory_checker dir = new Directory_checker();
         File[] files=dir.showFiles("Directory");
         double [][] matrix = read.read_files();
-
         for(int i=0; i<files.length; i++){
-            Correlation_matrix.add(new Label(""+i), 0, i+1);
-            Correlation_matrix.add(new Label(""+i), i+1, 0);
-            Correlation_matrix.getColumnConstraints().addAll(new ColumnConstraints(50));
+            Correlation_matrix.add(new Label(files[i].getName()), 0, i+1);
+            Correlation_matrix.add(new Label(files[i].getName()), i+1, 0);
+            Correlation_matrix.getColumnConstraints().addAll(new ColumnConstraints(70));
+            Correlation_matrix.getRowConstraints().addAll(new RowConstraints(20));
             for(int j=0;j<files.length; j++){
                 Correlation_matrix.add(new Label(two_point.format(matrix[i][j])), i+1, j+1);
             }
